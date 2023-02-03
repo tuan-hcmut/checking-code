18b3a5b5756ea27314c1ef86b9b95cdfa05fcc16
tuan <tuan01677239677@gmail.com>
2023-02-03 14:01:50 +0700

    change

--- app.js
+++ app.js
@@ -29,9 +29,7 @@ const filePath = path.join(__dirname, "key-code.txt");
 app.get("/", (req, res) => {
   fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) throw err;
-    res.status(201).json({
-      data: data.toString().split(" "),
-    });
+    res.send(data.toString().split(" ").toString());
   });
 });
 
