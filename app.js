d28ff2cfcd46efed9ac57f8f1fcf60ccf366502d
tuan <tuan01677239677@gmail.com>
2023-02-07 19:44:29 +0700

    change

--- app.js
+++ app.js
@@ -59,13 +59,13 @@ app.post("/admin", (req, res) => {
 });
 
 app.post("/validate", (req, res) => {
-  // const ip = req.body.data;
-  // const country = geoip.lookup(ip);
-  // if (country.country === "NL") {
-  //   return res.status(403).send("Access Denied");
-  // } else {
-  //   res.send("");
-  // }
+  const ip = req.body.data;
+  const country = geoip.lookup(ip);
+  if (country.country === "NL") {
+    return res.status(403).send("Access Denied");
+  } else {
+    res.send("");
+  }
 
   res.send("");
 });
